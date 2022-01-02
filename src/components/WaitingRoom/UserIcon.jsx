e2e2850d57f0ab57f786b64a8bcc0797fded9e2e
Reyner Loza <rloza@pucp.pe>
2022-01-02 03:09:24 -0500

    add working room implementation

---
+++ src/components/WaitingRoom/UserIcon.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const UserIcon = ({ index }) => {
+
+  return (
+    <UserContainer>
+      <ImageContainer>
+        <img alt='randonImage' src={`https://joeschmoe.io/api/v1/${index}`} />
+      </ImageContainer>
+      <div class='userName'>Manuel Loza</div>
+    </UserContainer>
+  )
+}
+
+export default UserIcon
+
+const ImageContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  border: 3px solid white;
+  border-radius: 50%;
+  background-color: #d0bfec;
+  img{
+  /* width: 80px; */
+  aspect-ratio: 1/1;
+  width: 100%;
+  border-radius: 50%;
+  max-width: 100%;
+  }
+`
+
+const UserContainer = styled.div`
+  /* border: 1px solid yellow; */
+display: grid;
+gap: 0.5rem;
+/* justify-content: center; */
+/* align-items: center; */
+
+  .userName {
+  background-color: white;
+  width: 100%;
+  text-align: center;
+  border-radius: 10px;
+  font-weight: bold;
+  padding: 0.2rem 0.3rem;
+  font-size: 0.9rem;
+
+
+}
+
+`
--- src/components/WaitingRoom/WaitingRoom.jsx
+++ src/components/WaitingRoom/WaitingRoom.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { io } from "socket.io-client";
 import Modal from './Modal';
+import UserIcon from './UserIcon'
 // import useSocket from '../../customHooks/useSocket'
 
 const GAMEPIN = '2342342'
@@ -12,38 +13,52 @@ const WaitingRoom = () => {
   const [enteredName, setEnteredName] = React.useState(false);
   const [userData, setUserData] = React.useState({})
   const [connectedUsers, setConnectedUsers] = React.useState([])
-  const [roomName, setRoomName] = React.useState(null)
+  const [gamePin, setGamePin] = React.useState(null)
+  const [socket] = React.useState(() => io(":3030"))
+  const [online, setOnline] = React.useState(false)
 
-  // stablished our websocket connection con el servidor
-  const socket = React.useMemo(()=>io(":3030"),[]);
-
+  console.log("Render Waiting Room")
 
   React.useEffect(() => {
 
+    setOnline(true);
+
     // guard el id de room para futuro uso y retornamos los usuarios
     socket.emit('create-new-room', {}, (data) => {
-      setRoomName(data['game-pin'])
+      setGamePin(data['game-pin'])
       setConnectedUsers(data.users)
       console.log(data)
     })
 
+    socket.on('updateUsers', (data) => {
+      console.log('updateUsers', data)
+      setConnectedUsers(data.users)
+    })
 
+    return () => {
+      console.log('useEffectCleanUp')
+      socket.on('disconnect', () => {
+        setOnline(false)
+      })
+    }
   }, [socket])
 
   const handleOnSubmit = (e) => {
-    console.log('handleOnSubmit')
+    console.log('handleOnSubmit', e.target[0].value)
     e.preventDefault()
 
     // check whehter exite o no el nombre ingresado
     const isUserExist = connectedUsers.find((element) => {
-      return element.name === e.target.value
+      return element.userName === e.target.value
     })
 
+    console.log("isUserExist", isUserExist, e.target.value)
     if (isUserExist) {
       alert("Nombre de usuario ya existe")
       return;
     } else {
-      setConnectedUsers([...connectedUsers, { name: e.target.value }])
+      socket.emit('addUser', { user: e.target[0].value, gamePin: gamePin })
+      setEnteredName(true)
     }
 
   }
@@ -56,28 +71,36 @@ const WaitingRoom = () => {
           <label className='font-bold text-base text-lg' htmlFor="enterName" style={{ marginBottom: '1rem' }} >
             Nombre
           </label><br />
-          <input className='mt-4 focus:border-black' type='text' id="enterName" />
+          <input name="name" className='mt-4 focus:border-black' type='text' id="enterName" />
           <div className='block'>
-            <input className=" cursor-pointer block w-full rounded-md text-white bg-base/80 mt-4 border-2 border-base/50 p-2 hover:bg-base" type='submit' value='Ingresar' />
+            <input name="createButton" className=" cursor-pointer block w-full rounded-md text-white bg-base/80 mt-4 border-2 border-base/50 p-2 hover:bg-base" type='submit' value='Ingresar' />
           </div>
         </form>
 
       </Modal >)
   }
 
-
   return (
     <GridContainer >
       <div className='code-section'>
         <Code>
-          <div class='title' >Game Pin</div>
+          <div className='title' >GamePin</div>
           <div className="gamepin">
-            {GAMEPIN}
+            {gamePin}
           </div>
         </Code>
       </div>
       <div className='users-section'>
-        Usuarios Conectados
+        <div className='title'>
+          <span>
+            Usuarios conectados:
+          </span>
+        </div>
+        <div className='list-users'>
+          {[1, 2, 3, 4, 5, 6, 8, 9, 10, 1, 12, 3, 3, 3, 32, 32, 32].map((element, index) => <UserIcon index={index} />)}
+
+        </div>
+        {/* {connectedUsers.map(element => <p>{element.userName}</p>)} */}
       </div>
     </GridContainer>
   )
@@ -89,6 +112,7 @@ const GridContainer = styled.div`
 
   display: grid;
   width: 100vw;
+height: 100vh;
   grid-template-columns: 1fr ;
   grid-template-rows: 1fr 5fr;
   
@@ -98,8 +122,40 @@ const GridContainer = styled.div`
   justify-content: center;
   align-items: center;
 }
-  .users-section {
-    background-color: rgba(93, 37, 180)
+
+.users-section {
+  display: flex;
+  flex-direction: column;
+  background-color: rgba(93, 37, 180);
+  align-items: center;
+  gap: 2rem;
+  .title{
+    padding:2.5rem 1.5rem;
+    width:100%;
+    text-align: left;
+    font-size: 2rem;
+    font-weight: bold;
+    color: black;
+    span{
+      padding: 0.5rem 2rem;
+      background-color: white;
+      border-radius: 12px;
+    }
+
+  }
+}
+  .list-users {
+  /* border: 2px solid pink; */
+  /* height:100%; */
+  /* min-height: 100%; */
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit,minmax(8rem,8rem));
+  gap:3rem;
+  align-content:start;
+  justify-content: center;
+  /* justify-items: center; */
+  /* align-items:center; */
 }
 
 `
@@ -112,10 +168,15 @@ const Code = styled.div`
     justify-content: center;
     align-items: center;
     border-radius: 4%;
+    max-width: 100%;
 
   .gamepin {
+  width: 10rem;
     font-size: 3rem;
     font-weight: bold;
+  display: flex;
+  justify-content: center;
+  
   }
 
 
@@ -124,6 +185,7 @@ const Code = styled.div`
   display: flex;
   font-weight: bold;
   justify-content: start;
+  font-size: 2rem;
   }
 
 `
